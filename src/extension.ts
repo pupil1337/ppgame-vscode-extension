3d0aad605b7e0f15ffe436f099dd38c18837df0b
pupil1337 <2500698263@qq.com>
2024-05-22 02:04:22 +0800

    设置 launch arg 的 configuration

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,13 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 class MyDebugConfigurationProvider implements vscode.DebugConfigurationProvider {
 
-	/**
-	 * Massage a debug configuration just before a debug session is being launched,
-	 * e.g. add all missing attributes to the debug configuration.
-	 */
 	 resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
-
-		// if launch.json is missing or empty
 		return (async () => {
-			const items = [
-				'--debug-collisions',
-				'--debug-paths',
-				'--debug-navigation',
-				'--debug-avoidance',
-				'--debug-canvas-item-redraw',
-				'--debug-stringnames',
-			];
+			const launch_args: string[] | undefined = vscode.workspace.getConfiguration('ppgame').get('launch_args');
+			const items = launch_args ?? [];
 			const options: vscode.QuickPickOptions & { canPickMany: true; } = { title: "args", canPickMany: true };
 			const selectedItems: string[] | undefined = await vscode.window.showQuickPick(items, options);
-
-
 			if(selectedItems) {
 				const args: string[] = config.args ?? [];
 				args.push(...selectedItems);
@@ -34,13 +18,9 @@ class MyDebugConfigurationProvider implements vscode.DebugConfigurationProvider 
 	}
 }
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ppgame" is now active!');
+
 	context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('cppvsdbg', new MyDebugConfigurationProvider()));
 }
 
