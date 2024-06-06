7cc32632177d961e5930747711f9963f6355b693
pupil1337 <2500698263@qq.com>
2024-06-06 16:34:11 +0800

    配置特定launch才使用额外参数列表

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,27 @@ class MyDebugConfigurationProvider implements vscode.DebugConfigurationProvider 
 
 	 resolveDebugConfiguration(folder: vscode.WorkspaceFolder | undefined, config: vscode.DebugConfiguration, token?: vscode.CancellationToken): vscode.ProviderResult<vscode.DebugConfiguration> {
 		return (async () => {
-			const launch_args: string[] | undefined = vscode.workspace.getConfiguration('ppgame').get('launch_args');
-			const items = launch_args ?? [];
-			const options: vscode.QuickPickOptions & { canPickMany: true; } = { title: "args", canPickMany: true };
-			const selectedItems: string[] | undefined = await vscode.window.showQuickPick(items, options);
-			if(selectedItems) {
-				const args: string[] = config.args ?? [];
-				args.push(...selectedItems);
+			const launch_args: Object[] | undefined = vscode.workspace.getConfiguration('ppgame').get('launch_args');
+			if (launch_args !== undefined) {
+				for (let i = 0; i < launch_args.length; ++i) {
+					const launch_arg: Object = launch_args[i];
+					if (launch_arg !== undefined && 'launchs' in launch_arg && 'args' in launch_arg) {
+						const launchs: string[] = launch_arg['launchs'] as any as string[];
+						if (launchs?.includes(config.name)) {
+							const args: string[] = launch_arg['args'] as any as string[];
+							if (args?.length > 0) {
+								const options: vscode.QuickPickOptions & { canPickMany: true; } = { title: "args", canPickMany: true };
+								const selectedItems: string[] | undefined = await vscode.window.showQuickPick(args, options);
+								if (selectedItems) {
+									const args: string[] = config.args ?? [];
+									args.push(...selectedItems);
+								}
+
+								return config;
+							}
+						}
+					}
+				}
 			}
 	
 			return config;
